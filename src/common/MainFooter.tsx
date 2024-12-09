83b3f495c802ce9722061093b2953bab4492546c
XxJMPPxX <151105147+XxJMPPxX@users.noreply.github.com>
2024-12-09 16:32:29 -0500

    FinalCloud

--- src/common/MainFooter.tsx
+++ src/common/MainFooter.tsx
@@ -1,11 +1,14 @@
-import './MainFooter.css'
+import './MainFooter.css';
+
 function MainFooter() {
     return (
         <footer id="main-footer">
             <div className="container">
-                2024 - TIL INC, todos los derechos reservados.
+                <p className="left">2024 - TIL INC, todos los derechos reservados.</p>
+                <p className="right">José María Ponce Porras - Diseño y Desarrollo de simuladores y videojuegos - C26</p>
             </div>
         </footer>
-    )
+    );
 }
-export default MainFooter
+
+export default MainFooter;
